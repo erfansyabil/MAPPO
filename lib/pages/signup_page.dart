920a0afdb409aa7f98b501cad1dc565eeb85ed3e
Amirulhani02 <amirulhani02@gmail.com>
2024-05-09 20:44:59 +0800

    minor update on signup page
    added images. back button to login

--- lib/pages/signup_page.dart
+++ lib/pages/signup_page.dart
@@ -4,29 +4,29 @@ class SignUpPage extends StatelessWidget {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
 
-  void signUp() {
-    // Implement your sign-up logic here
-    String email = emailController.text;
-    String password = passwordController.text;
-    // Perform sign-up operation using email and password
-    print('Signing up with Email: $email and Password: $password');
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromARGB(255, 174, 255, 216),
       appBar: AppBar(
         title: Text('Sign Up'),
       ),
       body: Padding(
-        padding: EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(20.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [
+          children: <Widget>[
+            Image.asset(
+              'lib/images/mappo.png',
+              height: 150,
+            ),
+            SizedBox(height: 20),
             TextField(
               controller: emailController,
               decoration: InputDecoration(
                 labelText: 'Email',
+                border: OutlineInputBorder(),
+                prefixIcon: Icon(Icons.email),
               ),
             ),
             SizedBox(height: 20),
@@ -34,13 +34,35 @@ class SignUpPage extends StatelessWidget {
               controller: passwordController,
               decoration: InputDecoration(
                 labelText: 'Password',
+                border: OutlineInputBorder(),
+                prefixIcon: Icon(Icons.lock),
               ),
               obscureText: true,
             ),
             SizedBox(height: 20),
             ElevatedButton(
-              onPressed: signUp,
+              onPressed: () {
+                // Add sign-up logic here
+              },
               child: Text('Sign Up'),
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text("Already have an account?"),
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context); // Navigate back to login page
+                  },
+                  child: Text(
+                    'Log In',
+                    style: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                    ),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
